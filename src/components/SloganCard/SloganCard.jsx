519c126d6162b50a20be5dd75f06e4d659446c66
Susanto <nerdyspook007@gmail.com>
2022-08-24 05:06:32 +0530

    feat: added copy to clipboard

--- src/components/SloganCard/SloganCard.jsx
+++ src/components/SloganCard/SloganCard.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
 import "./SloganCard.css";
 
 const SloganCard = ({ slogan }) => {
-  return <li className="card">{slogan}</li>;
+  const [isCopied, setIsCopied] = useState(false);
+
+  const copyText = async (text) => {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand("copy", true, text);
+    }
+  };
+
+  const handleClick = () => {
+    copyText(slogan)
+      .then(() => {
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 2000);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <li className="card_group" onClick={handleClick}>
+      <span className="tooltip">{isCopied ? "Copied!" : "Click to copy"}</span>
+      <p className="card">{slogan}</p>
+    </li>
+  );
 };
 
 export default SloganCard;
