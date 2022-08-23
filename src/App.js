91a433cc02a172ed535717c20a0a376978cca7e9
Susanto <nerdyspook007@gmail.com>
2022-08-23 16:37:32 +0530

    added results section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Results from "./components/Results/Results";
 
 const App = () => {
   return (
     <div className="App">
       <header>
         <Navbar className="navbar" />
+        <Results />
       </header>
     </div>
   );
---
+++ src/components/SloganCard/SloganCard.jsx
@@ -0,0 +1,7 @@
+import "./SloganCard.css";
+
+const SloganCard = ({ slogan }) => {
+  return <li className="card">{slogan}</li>;
+};
+
+export default SloganCard;
