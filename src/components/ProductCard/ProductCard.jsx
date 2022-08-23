00b0116fcdc0f0ecc2f794bd19e1303c49b7e37a
Susanto <nerdyspook007@gmail.com>
2022-08-23 19:58:21 +0530

    fix: product card title

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,7 +4,7 @@ const ProductCard = ({ image, title, details }) => {
   return (
     <div className="product_card">
       <img src={image} alt="privacy generator" />
-      <h4 className="heading">Privacy Policy Generator</h4>
+      <h4 className="heading">{title}</h4>
       <p>{details}</p>
     </div>
   );
